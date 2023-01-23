eabd9cf36f58aade212dfa4a1ad3f3b3e14cd01d
Charles <knappcharles@icloud.com>
2023-01-23 12:40:10 -0700

    Minor mobile responsive navbar fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { SearchBar } from "./";
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ borderBottom: '1px solid #242424', position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={42} style={{marginLeft: '2rem'}}/>
+      <img src={logo} alt="logo" height={42}/>
     </Link>
     <SearchBar />
   </Stack>
