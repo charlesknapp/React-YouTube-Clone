40456a4666f8d59261906157e6d1c531fffc7418
Charles <knappcharles@icloud.com>
2023-01-23 13:08:23 -0700

    Fixed mobile navbar indexing position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { logo } from "../utils/constants";
 import { SearchBar } from "./";
 
 const Navbar = () => (
-  <Stack direction="row" alignItems="center" p={2} sx={{ borderBottom: '1px solid #242424', position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+  <Stack direction="row" alignItems="center" p={2} sx={{ zIndex: 10, borderBottom: '1px solid #242424', position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={42}/>
     </Link>
